c9fb73df166abd54d53a9564db913d5fe09631fc
byGeek <byyang@mail.dlut.edu.cn>
2018-11-08 15:07:51 +0800

    dummy commit

--- src/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ src/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -2,9 +2,12 @@
 #demo test
 #import function from module
 from superfastcode import test_mul, test_sub
+import os
 
 result = test_mul(2,2)
 print(result);
 
 result = test_sub(5,1);
 print(result);
+
+os.system("pause");
